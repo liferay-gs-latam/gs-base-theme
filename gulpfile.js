7394035499a370315c7a70fe7713cc0524b9893f
Jordana Morais <jordana.morais@liferay.com>
2019-06-24 20:30:27 -0300

    Theme Webpack Config | Update gulp file with npm run build-webpack rule

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,28 @@
 
 var gulp = require('gulp');
 var liferayThemeTasks = require('liferay-theme-tasks');
+var exec = require('child_process').exec;
 
 liferayThemeTasks.registerTasks({
-	gulp,
+	gulp: gulp,
+	hookFn: function(gulp) {
+
+		gulp.hook('before:build:src', function(done){
+			exec('npm run build-webpack', function(error){
+
+				if(error !== null){
+					console.log(' ----', '\n', 
+								'Webpack is not installed!', '\n',
+								'Please run: "npm i webpack"', '\n',
+								'----');
+				} else {
+					done();
+					console.log(' ----', '\n',
+							'Webpack Production Mode ran successfully', '\n',
+							'----');
+				}
+
+			});
+		});
+	}
 });
